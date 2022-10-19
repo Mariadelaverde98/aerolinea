7e38a536cb9eb9940abfa5fb69b5fe03fedeaf85
Mariadelaverde98 <mariadelaverde98@gmail.com>
2022-10-19 12:56:03 +0200

    home

---
+++ home.js
@@ -0,0 +1,8 @@
+function buscarVuelos() {
+    var origen = document.getElementsByTagName('input')[0].value;
+    var destino = document.getElementsByTagName('input')[1].value;
+    var numPasajeros = document.getElementsByTagName('input')[2].value;
+    var fecha = document.getElementsByTagName('input')[3].value;
+    alert(fecha);
+    
+}
