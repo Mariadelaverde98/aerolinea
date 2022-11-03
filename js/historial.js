643f191a2c8c994d3217607444f46b336175a729
gmirg <gofthet@gmail.com>
2022-11-04 01:16:59 +0700

    terminado boton checking

--- js/historial.js
+++ js/historial.js
@@ -11,14 +11,9 @@ class Compra {
     }
 }
 // import { Vuelo } from './vuelo.js';
-
-Date.prototype.addHours = function () {
-    this.setTime(this.getTime() + (47 * 60 * 60 * 1000));
-    return this;
-}
-function subirHistorial() {
+(function subirHistorial() {
     let fecha = new Date(); // extraigo la fecha de hoy y le sumo 47 horas 
-    let fechaMenos48h = fecha.addHours();
+    let fechaMenos48h = sumar47(fecha);
     let dd = String(fechaMenos48h.getDate()).padStart(2, '0');
     let mm = String(fechaMenos48h.getMonth() + 1).padStart(2, '0');
     let yyyy = fechaMenos48h.getFullYear();
@@ -26,7 +21,7 @@ function subirHistorial() {
     fechaMenos48h = yyyy + '/' + mm + '/' + dd;
     let compra1 = new Compra(
         '0001', // Numero de reserva
-        '2022-10-28', // Fecha de reserva, aqui el dia de la compra
+        '28/10/2022', // Fecha de reserva, aqui el dia de la compra
         'gofthet@gmail.com',  // Aqui el correo del usuario
         [JSON.parse('{"nombre": "Gerardo", "Apellidos": "Mir Garcia", "dni": "52656007Z"}')], // Pasajeros
         [JSON.parse('{"id":"DA000","origen":"Madrid","destino":"Edimburgo","fecha":"","hora":"","horallegada":"10:15","asientosLibres":25,"asientosOcupados":["1A","2A","4C","1DC","2DC"],"precio":120}')]);
@@ -35,16 +30,19 @@ function subirHistorial() {
     console.log(compra1);
     let compra2 = new Compra(
         '0002', // Numero de reserva
-        '2022-10-28', // Fecha de reserva, aqui el dia de la compra
+        '28/10/2022', // Fecha de reserva, aqui el dia de la compra
         'gofthet@gmail.com',  // Aqui el correo del usuario
         [JSON.parse('{"nombre": "Gerardo", "Apellidos": "Mir Garcia", "dni": "52656007Z"}'), JSON.parse('{"nombre": "Gerar2", "Apellidos": "Mir Garcia", "dni": "52656008Z"}')],
         [JSON.parse('{"id":"DA001","origen":"Madrid","destino":"Paris","fecha":"2022/11/31","hora":"14:00","horallegada":"19:15","asientosLibres": 30,"asientosOcupados":[""],"precio":240}')]);
     console.log(compra1);
-    // localStorage.clear();
+    localStorage.removeItem('compras');
     var historial = [compra1, compra2];
     localStorage.setItem("compras", JSON.stringify(historial));
+})();
+function sumar47(date) {
+    date.setTime(date.getTime() + (47 * 60 * 60 * 1000));
+    return date;
 }
-
 function userHistorial() {
     let historial = JSON.parse(localStorage.compras);
     if (historial == null) { alert('Todavía no has hecho compras') };
@@ -61,17 +59,16 @@ function desplegarHistorial() {
         let numeroReserva = element.numReserva;
         let fechReserva = element.fechaReserva;
         let pasajes = element.numPasajes;
-        console.log("Esto es pasajes: " + pasajes[0]);
         let infoPasajeros = [];
         for (let i = 0; i < pasajes.length; i++) {
             if (infoPasajeros.length == 0) {
-                infoPasajeros.push(pasajes[i].nombre);
-                infoPasajeros.push(pasajes[i].Apellidos);
-                infoPasajeros.push(pasajes[i].dni);
+                infoPasajeros.push(pasajes[i].nombre + " " + pasajes[i].Apellidos);
+                // infoPasajeros.push(pasajes[i].Apellidos);
+                // infoPasajeros.push(pasajes[i].dni);
             } else {
-                infoPasajeros.push(pasajes[i].nombre);
-                infoPasajeros.push(pasajes[i].Apellidos);
-                infoPasajeros.push(pasajes[i].dni);
+                infoPasajeros.push(pasajes[i].nombre + " " + pasajes[i].Apellidos);
+                // infoPasajeros.push(pasajes[i].Apellidos);
+                // infoPasajeros.push(pasajes[i].dni);
             }
             console.log("infoPasajeros length: " + infoPasajeros.length);
         }
@@ -82,6 +79,7 @@ function desplegarHistorial() {
                 infoVuelos.push(vuelo[i].id);
                 infoVuelos.push(vuelo[i].destino);
                 infoVuelos.push(vuelo[i].fecha);
+                infoVuelos.push(vuelo[i].hora)
                 infoVuelos.push(vuelo[i].asientosLibres);
                 infoVuelos.push(vuelo[i].asientosOcupados);
                 infoVuelos.push(vuelo[i].precio);
@@ -89,16 +87,25 @@ function desplegarHistorial() {
                 infoVuelos.push(vuelo[i].id);
                 infoVuelos.push(vuelo[i].destino);
                 infoVuelos.push(vuelo[i].fecha);
+                infoVuelos.push(vuelo[i].hora);
                 infoVuelos.push(vuelo[i].asientosLibres);
                 infoVuelos.push(vuelo[i].asientosOcupados);
                 infoVuelos.push(vuelo[i].precio);
             }
         }
-        console.log(infoVuelos); 
+        let fechaVuelo = infoVuelos[2] + " " + infoVuelos[3];
+        let FechaVueloMs = new Date(fechaVuelo).getTime();
+        let FechaAhoraMS = Date.now();
+        let diferenciaFechas = (FechaVueloMs - FechaAhoraMS) / (1000 * 60 * 60);
+        console.log(FechaVueloMs);
+        console.log(FechaAhoraMS);
+        console.log(diferenciaFechas);
+        console.log(infoVuelos);
         let printReserva = `No. reserva: ${numeroReserva}`;
         let printFechaReserva = `Comprado el : ${fechReserva}`;
-        let printPagado = `Total compra: ${infoVuelos[5]} €`;
-        let printVuelo = `Número de vuelo: ${infoVuelos[0]}`
+        let printPagado = `Total compra: ${infoVuelos[6]} €`;
+        let printVuelo = `Número de vuelo: ${infoVuelos[0]}`;
+        let printDestino = `Destino :${infoVuelos[1]}`;
         let container = document.createElement('div');
         container.setAttribute('class', 'container card');
         var documentFragment = document.createDocumentFragment();
@@ -118,33 +125,63 @@ function desplegarHistorial() {
         let col3 = document.createElement('p');
         col3.setAttribute('class', 'col');
         let contenidoCol3 = document.createTextNode(printVuelo);
-        col3.appendChild(contenidoCol3);
-        console.log(infoPasajeros.join(" ")); // split y join
-        for (let i = 0; i < infoPasajeros.length; i++) {
-            let k = infoPasajeros.length;
-            console.log(infoPasajeros);
-            let contenidoCol3b = document.createTextNode(infoPasajeros[i]);
-            let parrafo = document.createElement('p');
-            parrafo.appendChild(contenidoCol3b);
-            col3.appendChild(parrafo);
-        }
-        let row3 = document.createElement('div');
-        row3.setAttribute('class', 'row');
         let col4 = document.createElement('div');
         col4.setAttribute('class', 'col right');
-        let contenidoCol4 = document.createTextNode(printPagado);
+        let contenidoCol4 = document.createTextNode(printDestino);
         col4.appendChild(contenidoCol4);
+        col3.appendChild(contenidoCol3);
+        let row3 = document.createElement('div');
+        row3.setAttribute('class', 'row');
+        let col5 = document.createElement('div');
+        col5.setAttribute('class', 'col pasajeros');
+        let titular = document.createElement('p');
+        let titPasajeros = document.createTextNode("Pasajeros");
+        titular.appendChild(titPasajeros);
+        col5.appendChild(titular);
+        let hr = document.createElement("hr");
+        col5.appendChild(hr);
+        for (let i = 0; i < infoPasajeros.length; i++) {
+            let cont = i + 1;
+            let contenidoCol5cont = document.createTextNode(cont + '. ' + infoPasajeros[i]);
+            let parrafo = document.createElement('p');
+            parrafo.appendChild(contenidoCol5cont);
+            if (diferenciaFechas < 48) {
+                let boton = document.createElement('button');
+                boton.setAttribute('class', 'btn btn-outline-success boton')
+                let textButton = document.createTextNode('Checking disponible')
+                boton.appendChild(textButton);
+                parrafo.appendChild(boton);
+            }
+            col5.appendChild(parrafo);
+        }
+        let row4 = document.createElement('div');
+        row4.setAttribute('class', 'row');
+        let col6 = document.createElement('div');
+        col6.setAttribute('class', 'col right');
+        let contenidoCol6 = document.createTextNode(printPagado);
+        col6.appendChild(contenidoCol6);
         var documentFragment2 = document.createDocumentFragment();
         documentFragment2.appendChild(row1);
         row1.appendChild(col1);
         row1.appendChild(col2);
         row2.appendChild(col3);
-        row3.appendChild(col4);
+        row2.appendChild(col4);
+        row3.appendChild(col5);
+        row4.appendChild(col6);
         container.appendChild(row1);
         container.appendChild(row2);
         container.appendChild(row3);
+        container.appendChild(row4);
         document.body.appendChild(container);
+        boton();
     });
 }
 
+function boton() {
+    const button = document.querySelector("button");
+    button.addEventListener("click", function () {
+        window.location = "checking.html"
+    });
+}
+   
 
